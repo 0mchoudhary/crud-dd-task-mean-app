d12d17a881f3bf27c98c7a7181a340cf46a624db
imomchoudharyy@gmail.com <130913798+0mchoudhary@users.noreply.github.com>
2026-02-24 12:08:04 +0530

    feat: enable CORS for frontend at port 8081

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
